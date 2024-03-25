189445d85d9b378aaa92443b86b3115899408356
suchir <suchirp14@gmail.com>
2024-03-25 18:51:56 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,97 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_community.vectorstores.faiss import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template 
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size = 1000,
+        chunk_overlap = 200,
+        length_function =len
+    )
+
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorestore = FAISS.from_texts(texts = text_chunks, embedding=embeddings)
+    return vectorestore
+
+def get_conversation_chain(vectorestore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever=vectorestore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2== 0:
+            st.write(user_template.replace("{{MSG}}",message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}",message.content), unsafe_allow_html=True)
+
+
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with pdfs",page_icon=":books:")
+
+    st.write(css,unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat with pdfs :books:")
+    user_question = st.text_input("Ask a question about your documents: ")
+    if user_question:
+        handle_userinput(user_question)
+
+
+    
+    with st.sidebar:
+        st.subheader("Your Documents")
+        pdf_docs = st.file_uploader("Upload PDF's Here and Click Process",accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                #get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                
+                #get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                
+                #create vector store
+                vectorstore = get_vector_store(text_chunks)
+
+                # conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+
+
+
+if __name__ == '__main__':
+    main()
